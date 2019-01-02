d47fa50749cf6a98d9a51e0c2638dca97dd03351
mohsan <mohsinsultan.pk@gmail.com>
2019-01-02 21:19:43 +0500

    02-01-2019

--- app/src/main/java/com/example/moo_chat/moochat/MainActivity.java
+++ app/src/main/java/com/example/moo_chat/moochat/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.moo_chat.moochat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -11,12 +14,19 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("MyMooChat");
+
     }
 
     @Override
@@ -27,10 +37,32 @@ public class MainActivity extends AppCompatActivity {
 
         if (currentUser == null)
         {
-            Intent loginIntent = new Intent(MainActivity.this , LoginActivity.class);
-            startActivity(loginIntent);
-            finish();
+          GoToStart();
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         super.onCreateOptionsMenu(menu);
+         getMenuInflater().inflate(R.menu.main_menu , menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+         super.onOptionsItemSelected(item);
+
+         if (item.getItemId() == R.id.main_logout_btn) {
+             FirebaseAuth.getInstance().signOut();
+             GoToStart();
+         }
+        return true;
+    }
+
+    private void GoToStart() {
+        Intent loginIntent = new Intent(MainActivity.this , LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
+    }
 }
--- app/src/main/java/com/example/moo_chat/moochat/RegisterActivity.java
+++ app/src/main/java/com/example/moo_chat/moochat/RegisterActivity.java
@@ -1,10 +1,12 @@
 package com.example.moo_chat.moochat;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,21 +19,34 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Objects;
+
 public class RegisterActivity extends AppCompatActivity implements  View.OnClickListener{
 
     EditText regName, regEmail, regPassword;
     Button RegAcBtn;
 
+    private Toolbar mToolbar;
+
     //Firebase
     private FirebaseAuth mAuth;
 
+
     // ---
 
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         mAuth = FirebaseAuth.getInstance();
+
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("MyMooChat");
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
+
         // Check Internet connection here. ---- code latter.
         initField();
     }
@@ -69,19 +84,16 @@ public class RegisterActivity extends AppCompatActivity implements  View.OnClick
 
     private void registerUser(String displayName, String displayEmail, String displayPassword) {
 
-        mAuth.createUserWithEmailAndPassword(displayEmail, displayPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
+        mAuth.createUserWithEmailAndPassword(displayEmail, displayPassword).addOnCompleteListener(task -> {
 
-                if (task.isComplete()){
-                    Intent regIntent = new Intent(RegisterActivity.this , MainActivity.class);
-                    startActivity(regIntent);
-                    finish();
-                } else {
-                    Toast.makeText(RegisterActivity.this, "You are having some error.", Toast.LENGTH_SHORT).show();
-                }
-
+            if (task.isComplete()){
+                Intent regIntent = new Intent(RegisterActivity.this , MainActivity.class);
+                startActivity(regIntent);
+                finish();
+            } else {
+                Toast.makeText(RegisterActivity.this, "You are having some error.", Toast.LENGTH_SHORT).show();
             }
+
         });
 
 
